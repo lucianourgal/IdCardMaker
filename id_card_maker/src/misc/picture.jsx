d7d8c6ea1f015ddd7bb19439b576c93f32d5eb63
lucianourgal <lucianourgal@hotmail.com>
2019-02-12 11:19:20 -0200

    Id image zoom
    Still needs to be a custom image

--- id_card_maker/src/misc/picture.jsx
+++ id_card_maker/src/misc/picture.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./picturebox.css";
 
-export default props => <div className="picturebox">{props.src}</div>
+export default props => <div className="picturebox"><img src={require("../customimg/Name nmb 1.JPG")}/></div>
 //<div className="picturebox">{props.src}</div>
 //<div className="picturebox"><img src={require('../customimg/' + props.src + '.jpg')}/></div>
