63c2283d3a26093ef4dfc4ce58482f970fa2f1e6
lucianourgal <lucianourgal@hotmail.com>
2019-02-14 13:55:10 -0200

    Nickname inclusion

--- id_card_maker/src/cards/front2.jsx
+++ id_card_maker/src/cards/front2.jsx
@@ -8,12 +8,14 @@ export default class front2 extends Component {
 
     render() {
 
+        const nicknam = this.props.nick ? this.props.nick : this.props.name.split(" ")[0];
+
         return (
             <div className="card front2">
                 <img src={require("../img/IFPRheader.PNG")} alt={"IFPR logo"}/>
                 <p>IDENTIDADE ESTUDANTIL</p>
                 <Picture src={this.props.name}/>
-                <Textfield text={this.props.name.split(" ")[0]} label={"Nome"}/>
+                <Textfield text={nicknam} label={"Nome"}/>
                 <Textfield text={this.props.course} label={"Curso"} large={true}/>
             </div>
         )
