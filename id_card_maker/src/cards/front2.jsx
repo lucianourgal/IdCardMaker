026c6a5dc3c88812c8395515dee2715680564d50
lucianourgal <lucianourgal@hotmail.com>
2019-02-27 14:03:17 -0300

    Large registry number position fix

--- id_card_maker/src/cards/front2.jsx
+++ id_card_maker/src/cards/front2.jsx
@@ -10,13 +10,14 @@ export default class front2 extends Component {
 
         let nicknam = this.props.nick ? this.props.nick : this.props.name.split(" ")[0];
         nicknam = nicknam.toUpperCase();
+        const registryClasses = this.props.registry.length > 15 ? "personRegistryField smallfont" : "personRegistryField";
 
         return (
             <div className="card front2">
                 <img src={require("../img/IFPRheader.PNG")} alt={"IFPR logo"}/>
                 <p>IDENTIDADE ESTUDANTIL</p>
                 <Picture src={this.props.name}/>
-                <div className="personRegistryField">{this.props.registry}</div>
+                <div className={registryClasses}>{this.props.registry}</div>
                 <Textfield text={nicknam} label={"Nome"}/>
                 <Textfield text={this.props.course} label={"Curso"} large={true}/>
             </div>
