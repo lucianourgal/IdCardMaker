1d68c89e9e2ca9f1c1f75debeff2cee6cb308010
lucianourgal <lucianourgal@hotmail.com>
2023-04-01 15:34:30 -0300

    translate to portuguese and update packages

--- id_card_maker/src/App.js
+++ id_card_maker/src/App.js
@@ -55,7 +55,8 @@ class App extends Component {
   }
 
   generateStateChanger = (labelx, func, val) => {
-    return <div className="princChoose">{labelx}: 
+    return <div className="princChoose">
+              <span>{labelx}:</span> 
               <input type="checkbox" onChange={func} value={val}/>
             </div>
   }
@@ -70,23 +71,23 @@ class App extends Component {
           :
           <div>
             <header>
-              <h1>Id card maker</h1>
-              <p>Software developed by Luciano Urgal Pando</p>
+              <h1><a href="https://github.com/lucianourgal/IdCardMaker" target="_blank" rel="noopener noreferrer">Id card maker</a></h1>
+              <p>Desenvolvido por Luciano Urgal Pando</p>
               <p>Please donate Bitcoin (BTC) to address bc1qzwlp2yzl783pskg9vrueuwhda7lrfmg3gc6enl</p>
             </header>
-            <h3>Input the persons data in the following format:</h3>
-            <p>Name, course, expireDate, (OPTIONAL:) Document, birthDate, registryNumber-RA, nickName</p>
+            <h3>Insira os dados pessoas no seguinte formato:</h3>
+            <p>Nome, curso, data_expiração, (opcional) documento, data_nascimento, registro_acadêmico_RA, (opcional) apelido</p>
 
-            <div className="princChoose">Principal name:_________
+            <div className="princChoose">
+              <span>Nome do diretor:</span>
               <input label="Principal" type="text" value={this.state.principal} onChange={this.principalChangeHandler}/>
             </div>
 
-            { this.generateStateChanger("Card type B", this.typeBToggleHandler, this.state.typeB) }
-            { this.generateStateChanger("Hide cards", this.hideCardsToggleHandler, this.state.hideCards) }
-            { this.generateStateChanger("Hide recieving lists", this.hideRecieveListsToggleHandler, this.state.hideRecieveLists) }
+            { this.generateStateChanger("Ocultar carteirinhas", this.hideCardsToggleHandler, this.state.hideCards) }
+            { this.generateStateChanger("Ocultar listas de recebimento", this.hideRecieveListsToggleHandler, this.state.hideRecieveLists) }
 
             <textarea value={this.state.text} onChange={this.txtChange}/>
-            <button onClick={this.generatePages}>Generate Id cards</button>
+            <button onClick={this.generatePages}>Gerar carteirinhas</button>
           </div> }
       </div>
     );
